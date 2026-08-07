c5e256538f1fc4b563a0888d6413536206dce442
Sulinka Lara Miliano <sulinkalara01@gmail.com>
2026-08-07 15:03:11 -0400

    Filter products by category in SQL query
    Added a WHERE clause to filter products by specific categories.

--- ROW_NUMBER  + WHERE IN.sql
+++ ROW_NUMBER  + WHERE IN.sql
@@ -1,3 +1,5 @@
+-- Rank products by stock quantity in descending order within specific categories--
+
 SELECT stock_quantity, 
        category,
 	   ROW_NUMBER() OVER(ORDER BY category ASC, stock_quantity DESC)
