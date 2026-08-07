3068ad2003503ccc2be6c45040de2eca2b57994c
Sulinka Lara Miliano <sulinkalara01@gmail.com>
2026-08-07 14:53:22 -0400

    Add files via upload
    -- Assign a unique sequential number to each distinct product category sorted alphabetically

---
+++ ROW_NUMBER + SUBQUERY IN FROM.sql
@@ -0,0 +1,8 @@
+Select category,
+  ROW_NUMBER() OVER () AS number_of_unique_category
+FROM(
+   SELECT DISTINCT category
+   FROM products
+   ORDER BY category ASC
+) AS categories_subquery
+ORDER BY category ASC;
